68b1b832775ba16289b1a62e81b2832acc110bb1
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-02-25 23:43:07 -0300

    Release hook when closing

--- src/main/java/com/timer_san/EntryPoint.java
+++ src/main/java/com/timer_san/EntryPoint.java
@@ -1,25 +1,45 @@
 package com.timer_san;
 
 import com.timer_san.timer.scene.TimerSceneManager;
+import io.reactivex.annotations.NonNull;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import lc.kra.system.keyboard.GlobalKeyboardHook;
 
 /**
  * Created by saguilera on 2/25/17.
  */
 public class EntryPoint extends Application {
 
+    @NonNull
+    private static EntryPoint instance;
+    @NonNull
+    private GlobalKeyboardHook systemHook;
+
     public static void main(String[] args) {
         launch(args);
     }
 
+    public static EntryPoint getInstance() {
+        return instance;
+    }
+
     public void start(Stage stage) throws Exception {
+        instance = this;
+        systemHook = new GlobalKeyboardHook();
+
         stage.setAlwaysOnTop(true);
         stage.setResizable(false);
         stage.setTitle("Bkw Timer-San Ｏ(≧▽≦)Ｏ");
 
         stage.setScene(new TimerSceneManager().scene());
         stage.show();
+
+        systemHook.shutdownHook();
+    }
+
+    public GlobalKeyboardHook getSystemHook() {
+        return systemHook;
     }
 
 }
